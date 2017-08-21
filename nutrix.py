eada76bebe8beb1ba717d2a4a39504c05e915974
julia kim-chung <julhkm@gmail.com>
2017-08-21 10:23:04 -0500

    Add files via upload

--- nutrix.py
+++ nutrix.py
@@ -16,7 +16,22 @@ def products():
     json_object = r.json()
     my_object = json_object['hits']
     product_num = len(my_object)
-    return render_template('juice.html', product_name= my_object)
+    one_ounce = 0.03
+    for item in my_object:
+        item_name = item['fields']['item_name']
+        item_calorie = item['fields']['nf_calories']
+        item_gram = item['fields']['nf_serving_weight_grams']
+        item_ingredient = item['fields']['nf_ingredient_statement']
+        try :
+            fluid_ounce =float(item_gram * one_ounce)
+            average_calorie = float(item_calorie/fluid_ounce)
+            
+        except:
+             item_gram='null'
+        # print(item_name)
+        # print(item_calorie)
+        # print(average_calorie)
+    return render_template('juice.html', product_name= item_name, product_calories=item_calorie, product_ingredient= item_ingredient, average_calorie=average_calorie)
 
 if __name__ == '__main__':
   app.run(debug=True)
